46c332a2fe9b886e6930b89a64b38b3fb96fc48f
suriya <suriyakumar.d@gmail.com>
2025-01-01 21:02:56 +0530

    1st checkin

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const headings = <h1> Hello World !!😆</h1>;
+const headings = <h1> Hello World from Suriya !!😆</h1>;
 
 root.render(headings);
