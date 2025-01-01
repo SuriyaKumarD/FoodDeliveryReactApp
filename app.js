9c9dc3bd5fc318f67ce065c78fc02c449d8477be
suriya <suriyakumar.d@gmail.com>
2025-01-01 20:36:40 +0530

    React App Dev Changes

---
+++ app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const headings = <h1> Hello World !!😆</h1>;
+
+root.render(headings);
