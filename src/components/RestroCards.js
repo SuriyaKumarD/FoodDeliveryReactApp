0dddca49053e6c570b773cdc5d55dd383f828480
suriya <suriyakumar.d@gmail.com>
2025-01-25 12:26:58 +0530

    Font Name changes

--- src/components/RestroCards.js
+++ src/components/RestroCards.js
@@ -10,12 +10,12 @@ const RestroCards = (props) => {
     <div className="RestroCards w-53 h-[420px] p-4 m-2 bg-fuchsia-100 rounded-2xl hover:bg-amber-50 cursor-pointer hover:border drop-shadow-2xl">
       <div>
         <img
-          className="foodLogo rounded-2xl my-1 p-1 h-50 w-53"
+          className="foodLogo rounded-2xl my-1 p-1 h-50 w-53 drop-shadow-2xl"
           src={CLOUD_IMG_URL + cloudinaryImageId}
           alt="foodimg"
         ></img>
       </div>
-      <h3 className="">{name}</h3>
+      <h3 className="font-mono">{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h3>{avgRating}</h3>
       <h3>{locality}</h3>
